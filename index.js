8e099acddab0f53fe7f9ad7b9db395e5763ccbeb
naari3 <naari.named@gmail.com>
2019-08-25 18:45:22 +0900

    Make repeat

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-module.exports = async (req, res) => {
+const { router, get } = require('microrouter')
+
+const makeSad = (count) => `（${'；＿'.repeat(count * 6 > 268435440 ? 44739240 : count).slice(0, -1)}）`
+
+const parsePositiveInt = (str) => Math.abs(parseInt(str, 10))
+
+const semi = async (req, res) => {
   res.setHeader('Content-type', 'text/plain; charset=UTF-8')
-  res.end('（；＿；）')
+  res.end(makeSad(parsePositiveInt(req.params.count) || 2))
 }
+
+module.exports = router(get('/:count', semi), get('/*', semi))
