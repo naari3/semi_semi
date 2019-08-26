d780e1388c8d9bec36e12d20539be5d71301760c
naari3 <naari.named@gmail.com>
2019-08-26 14:06:18 +0900

    Simplified

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const { router, get } = require("microrouter");
 
-const makeSad = count =>
-  `（${"；＿".repeat(Math.min(777777, count)).slice(0, -1)}）`;
-
 const parsePositiveInt = str => Math.abs(parseInt(str, 10));
 
 const memo = {};
-const memoize = fn => {
-  return arg => {
-    const count = Math.min(777777, arg);
-    if (memo[count]) {
-      return memo[count];
-    } else {
-      const result = fn(count);
-      memo[count] = result;
-      return result;
-    }
-  };
+const makeSad = count => {
+  const validatedCount = Math.min(777777, count);
+  if (memo[validatedCount]) {
+    return memo[validatedCount];
+  } else {
+    const result = `（${"；＿".repeat(validatedCount).slice(0, -1)}）`;
+    memo[validatedCount] = result;
+    return result;
+  }
 };
-
-const memoizedMakeSad = memoize(makeSad);
 
 const semi = async (req, res) => {
   res.setHeader("Content-type", "text/plain; charset=UTF-8");
-  res.end(memoizedMakeSad(parsePositiveInt(req.params.count) || 2));
+  res.end(makeSad(parsePositiveInt(req.params.count) || 2));
 };
 
 module.exports = router(get("/:count", semi), get("/*", semi));
