72371a7d7a19d1b3780d9b3c2058ed77c52ffe7c
naari3 <naari.named@gmail.com>
2019-08-26 11:54:34 +0900

    Add devDependency prettier

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const { router, get } = require('microrouter')
+const { router, get } = require("microrouter");
 
-const makeSad = (count) => `（${'；＿'.repeat(Math.min(777777, count)).slice(0, -1)}）`
+const makeSad = count =>
+  `（${"；＿".repeat(Math.min(777777, count)).slice(0, -1)}）`;
 
-const parsePositiveInt = (str) => Math.abs(parseInt(str, 10))
+const parsePositiveInt = str => Math.abs(parseInt(str, 10));
 
-const memo = {}
-const memoize = (fn) => {
-  return (arg) => {
-    const count = Math.min(777777, count)
+const memo = {};
+const memoize = fn => {
+  return arg => {
+    const count = Math.min(777777, count);
     if (memo[count]) {
-      return memo[count]
+      return memo[count];
     } else {
-      const result = fn(count)
-      memo[count] = result
-      return result
+      const result = fn(count);
+      memo[count] = result;
+      return result;
     }
-  }
-}
+  };
+};
 
-const memoizedMakeSad = memoize(makeSad)
+const memoizedMakeSad = memoize(makeSad);
 
 const semi = async (req, res) => {
-  res.setHeader('Content-type', 'text/plain; charset=UTF-8')
-  res.end(memoizedMakeSad(parsePositiveInt(req.params.count) || 2))
-}
+  res.setHeader("Content-type", "text/plain; charset=UTF-8");
+  res.end(memoizedMakeSad(parsePositiveInt(req.params.count) || 2));
+};
 
-module.exports = router(get('/:count', semi), get('/*', semi))
+module.exports = router(get("/:count", semi), get("/*", semi));
